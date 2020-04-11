76e86e360c05d732648227d773cee40ecc9b9f30
kristinakuzenko <k.kuzenko@gmail.com>
2020-04-12 00:09:36 +0300

    Google maps finished

--- Frontend/src/order.js
+++ Frontend/src/order.js
@@ -5,6 +5,10 @@ var PizzaSize = {
     Small: "small_size"
 
 };
+var map;
+var point;
+var directionsDisplay;
+var newMarker;
 var api = require('./API');
 //Змінна в якій зберігаються перелік піц в кошику
 var Cart = [];
@@ -71,7 +75,33 @@ function checkWord() {
         return true;
     }
 }
-
+function checkAddress(){
+    geocodeAddress($("#inputAddress").val(), function(err, data1){
+        if(!err){
+            if(newMarker)newMarker.setMap(null);
+            newMarker	=	new	google.maps.Marker({
+                position: data1,
+                map: map,
+                icon:"assets/images/home-icon.png"
+            });
+            calculateRoute(point, data1, function(err, data){
+                if(!err){
+                    $("#time").text(data.duration);
+                }
+                else console.log(err);
+            })
+            $(".address-group").removeClass("has-error").addClass("has-success");
+            $(".address-group").find(".help-block").css("display", "none");
+            $("#address").text($("#inputAddress").val());
+            return true;
+        }
+        else{
+            $(".address-group").removeClass("has-success").addClass("has-error");
+            $(".address-group").find(".help-block").css("display", "inline-block");
+            return false;
+        }
+    });
+}
 $(document).keyup(function(event) {
     if ($("#inputPhone").is(":focus") && event.key == "Enter") {
         checkPhone();
@@ -80,10 +110,13 @@ $(document).keyup(function(event) {
     if ($("#inputName").is(":focus") && event.key == "Enter") {
         checkWord();
     }
+    if ($("#inputAddress").is(":focus") && event.key == "Enter") {
+        checkAddress();
+    }
 });
 
 $("#forward").click(function() {
-    if (checkWord() && checkPhone()) {
+    if (checkWord() && checkPhone()&&checkAddress()) {
         api.createOrder({
 
             name: $("#inputName").val(),
@@ -97,26 +130,24 @@ $("#forward").click(function() {
 
     }
 });
-function	initialize()	{
+function initialize()	{
+    directionsDisplay = new google.maps.DirectionsRenderer();
     //Тут починаємо працювати з картою
     var mapProp =	{
     center:	new	google.maps.LatLng(50.464379,30.519131),
     zoom:	11
     };
-    var html_element =	document.getElementById("googleMap");
-    var map	=	new	google.maps.Map(html_element,	 mapProp);
+var html_element =	document.getElementById("googleMap");
+map	=new google.maps.Map(html_element,	 mapProp);
     //Карта створена і показана
-    var point	=	new	google.maps.LatLng(50.464379,30.519131);
-    var marker	=	new	google.maps.Marker({
-    position:	point,
-    map: map,
-    icon:"assets/images/map-icon.png"
-    });
-    google.maps.event.addListener(map,	
-        'click',function(me){
-        var coordinates	=	me.latLng;
-
+    directionsDisplay.setMap(map);
+point	=	new	google.maps.LatLng(50.464379,30.519131);
+var marker	=	new	google.maps.Marker({
+position:	point,
+map: map,
+icon:"assets/images/map-icon.png"
 });
+
 function	geocodeLatLng(latlng,	 callback){
         //Модуль за роботу з адресою
         var geocoder	=	new	google.maps.Geocoder();
@@ -129,45 +160,63 @@ function	geocodeLatLng(latlng,	 callback){
         }
         });
  }
- google.maps.event.addListener(map,	
-    'click',function(me){
+google.maps.event.addListener(map,'click',function(me){
     var coordinates	=	me.latLng;
     geocodeLatLng(coordinates,	function(err,	adress){
     if(!err)	{
-    //Дізналися адресу
-    console.log(adress);
+        $("#inputAddress").val(adress);
+        $("#address").text(adress);
+        if(newMarker)newMarker.setMap(null);
+                newMarker	=	new	google.maps.Marker({
+                    position: coordinates,
+                    map: map,
+                    icon:"assets/images/home-icon.png"
+                });
+                calculateRoute(point, coordinates, function(err, data){
+                    if(!err){
+                        $("#time").text(data.duration);
+                    }
+                    else console.log(err);
+                })
     }	else	{
-    console.log("Немає адреси")
+    console.log(err)
     }
     })
-    });
-    function	geocodeAddress(adress,	 callback)	{
-        var geocoder	=	new	google.maps.Geocoder();
-        geocoder.geocode({'address':	address},	function(results,	status)	{
-        if	(status	===	google.maps.GeocoderStatus.OK&&	results[0])	{
-        var coordinates	=	results[0].geometry.location;
-        callback(null,	coordinates);
-        }	else	{
-        callback(new	Error("Can	not	find	the	adress"));
-        }
-        });
-        }    
+});  
 }
-function	calculateRoute(A_latlng,	 B_latlng,	callback)	{
+function geocodeAddress(address, callback)	{
+    var geocoder=new	google.maps.Geocoder();
+    geocoder.geocode({'address':address},function(results,	status)	{
+    if(status	===	google.maps.GeocoderStatus.OK&&	results[0])	{
+    var coordinates	=results[0].geometry.location;
+    callback(null,	coordinates);
+    }else{
+    callback(new	Error("Can	not	find	the	adress"));
+    }
+  });
+}
+function calculateRoute(A_latlng,	 B_latlng,	callback) {
     var directionService =	new	google.maps.DirectionsService();
     directionService.route({
-    origin:	A_latlng,
-    destination:	B_latlng,
-    travelMode:	google.maps.TravelMode["DRIVING"]
-    },	function(response,	status)	{
-    if	(	status	==	google.maps.DirectionsStatus.OK )	{
-    varleg	=	response.routes[	0	].legs[	0	];
-    callback(null,	{
-    duration:	leg.duration
+        origin:	A_latlng,
+        destination:	B_latlng,
+        travelMode:	google.maps.TravelMode["DRIVING"]
+    }, function(response,	status)	{
+        if	(status	==	google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            directionsDisplay.setOptions({
+                suppressMarkers: true,
+                preserveViewport: true
+              });
+            var leg	=	response.routes[0].legs[0];
+            callback(null,	{
+                route: response,
+                duration:	leg.duration.text
+            });
+        }	else	{
+            callback(new	Error("Can't find direction"));
+        }
     });
-    }	else	{
-    callback(new	Error("Can'	not	find	direction"));
-    }
-    });
-    } 
+}
+
  
